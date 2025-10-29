fdd8dc3a3928c8e7471d284f7b64c199504c93ae
affandisy <syihabuddin.affandi@gmail.com>
2025-10-30 02:36:53 +0700

    feat(go): menambahkan package yang dibutuhkan

---
+++ pkg/response/response.go
@@ -0,0 +1,31 @@
+package response
+
+type SuccessResponse struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+type ErrorResponse struct {
+	Success bool   `json:"success"`
+	Error   string `json:"error"`
+	Message string `json:"message"`
+	Details any    `json:"details,omitempty"`
+}
+
+func Success(message string, data any) SuccessResponse {
+	return SuccessResponse{
+		Success: true,
+		Message: message,
+		Data:    data,
+	}
+}
+
+func Error(err string, message string, details any) ErrorResponse {
+	return ErrorResponse{
+		Success: false,
+		Error:   err,
+		Message: message,
+		Details: details,
+	}
+}
